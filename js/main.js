c68e0f9835fd385f1fbcbc32cfb11e0c18c35ec9
Ronaldo <ronaldomorales.ai@gmail.com>
2026-01-19 14:54:20 -0500

    Updated index.html and main.js

--- js/main.js
+++ js/main.js
@@ -2,41 +2,41 @@ document.querySelector('button').addEventListener('click', submit)
 const myBalance = document.querySelector('.myBalance')
 const from = document.querySelector('.for')
 
-class Balance {
+function submit() {
+  const option = document.querySelector('select').value
+  const amount = document.querySelector('.amount').value
+
+  if (option === 'income') {
+    myAccount.income(Number(amount))
+    myAccount.history(`+$${amount}`)
+  } else if (option === 'expense') {
+    myAccount.expesne(Number(amount))
+    myAccount.history(`-$${amount}`)
+  }
+}
+
+class Account {
   constructor(current) {
     this.current = current
   }
 
   income(number) {
     this.current += number
-    myBalance.textContent = `$${this.current}`
-    this.history(number) 
+    myBalance.textContent = `$${this.current}` 
   }
 
   expesne(number) {
     this.current -= number
-    myBalance.textContent = `$${this.current}`
-    this.history(number) 
+    myBalance.textContent = `$${this.current}` 
   }
 
   history(earnOrSpent) {
     const history = document.querySelector('.history')
     const p = document.createElement('p')
 
-    p.textContent = `${from.value}: $${earnOrSpent}`
+    p.textContent = `${from.value.toUpperCase()}: ${earnOrSpent}`
     history.append(p)
   }
 }
 
-const account = new Balance(0)
-
-function submit() {
-  const option = document.querySelector('select').value
-  const amount = document.querySelector('.amount')
-
-  if (option === 'income') {
-    account.income(Number(amount.value))
-  } else if (option === 'expense') {
-    account.expesne(Number(amount.value))
-  }
-}
+ const myAccount = new Account(0)
